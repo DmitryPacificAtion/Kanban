31d0ea7492cc569bfff320e90db762d257544f33
DmitryPacificAtion <bruslik.dmitry@gmail.com>
2020-05-17 01:53:14 +0300

    Adaptation content

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 import './index.scss';
-console.log("Hello");
+// window.addEventListener('resize', resizeHandler);
+(function(){
+  resizeHandler();
+})();
+
+function resizeHandler () {
+  const windowHeight = window.outerHeight > 1080 ? 1080 : window.outerHeight;
+  const notes = document.getElementsByClassName('notes');
+  const columns = document.getElementsByClassName('column');
+  const wrapper = document.getElementsByClassName('columns-wrapper');
+  // if(wrapper.offsetHeight < 1080) {
+  //   document.body.setAttribute('style', 'background-size: auto')
+  // } else {
+  //   document.body.removeAttribute('style')
+  // }
+  // Object.values(notes).forEach(col => {
+  //   col.setAttribute('style', `height: ${col.offsetHeight - 40 - 16 - 30 - 16}px;`);
+  // })
+  // Object.values(columns).forEach(col => {
+  //   col.setAttribute('style', `height: ${windowHeight - 40 - 16}px`);
+  // })
+}
