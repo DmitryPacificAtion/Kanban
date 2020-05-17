6a9f1804d2257687144725e02435b760d5b0da45
DmitryPacificAtion <bruslik.dmitry@gmail.com>
2020-05-17 04:03:32 +0300

    Add effect on buttons, add create column and note

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 import './index.scss';
 // window.addEventListener('resize', resizeHandler);
 (function(){
-  resizeHandler();
+  function addColumn() {
+    
+  };
 })();
 
-function resizeHandler () {
-  const windowHeight = window.outerHeight > 1080 ? 1080 : window.outerHeight;
-  const notes = document.getElementsByClassName('notes');
-  const columns = document.getElementsByClassName('column');
-  const wrapper = document.getElementsByClassName('columns-wrapper');
-  // if(wrapper.offsetHeight < 1080) {
-  //   document.body.setAttribute('style', 'background-size: auto')
-  // } else {
-  //   document.body.removeAttribute('style')
-  // }
-  // Object.values(notes).forEach(col => {
-  //   col.setAttribute('style', `height: ${col.offsetHeight - 40 - 16 - 30 - 16}px;`);
-  // })
-  // Object.values(columns).forEach(col => {
-  //   col.setAttribute('style', `height: ${windowHeight - 40 - 16}px`);
-  // })
-}
+// function resizeHandler () {
+//   const windowHeight = window.outerHeight > 1080 ? 1080 : window.outerHeight;
+//   const notes = document.getElementsByClassName('notes');
+//   const columns = document.getElementsByClassName('column');
+//   const wrapper = document.getElementsByClassName('columns-wrapper');
+// }
