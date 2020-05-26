82cbfeb145a75469bd2aad5d2e76c34ba0910c5c
DmitryPacificAtion <bruslik.dmitry@gmail.com>
2020-05-27 02:10:12 +0300

    Set up prod and dev builds

--- webpack.config.js
+++ webpack.config.js
@@ -5,79 +5,155 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 // Tip: Try to use without destructuring, if node -v less then 10
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-module.exports = {
-  mode: "production",
-  entry: {
-    app: "./src/index.js",
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/"
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        styles: {
-          name: 'styles',
-          test: /\.css$/,
-          chunks: 'all',
-          enforce: true,
+module.exports = (env, argv) => {
+  let config = {};
+  if (argv.mode === 'development') {
+    config = {
+      mode: "development",
+      entry: {
+        app: "./src/index.js",
+      },
+      output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/"
+      },
+      optimization: {
+        splitChunks: {
+          cacheGroups: {
+            styles: {
+              name: 'styles',
+              test: /\.css$/,
+              chunks: 'all',
+              enforce: true,
+            },
+          },
         },
       },
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        exclude: "/node_modules/",
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader,
-          "css-loader",
+      module: {
+        rules: [
           {
-            loader: "sass-loader",
+            test: /\.js?$/,
+            exclude: "/node_modules/",
+            use: ["babel-loader"],
           },
-          {loader: "required-loader"}
-        ],
+          {
+            test: /\.scss$/,
+            use: [MiniCssExtractPlugin.loader,
+              "css-loader",
+              {
+                loader: "sass-loader",
+              },
+              {loader: "required-loader"}
+            ],
+          },
+          {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ["file-loader"],
+          },
+          {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: ["file-loader"],
+          }
+        ]
       },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"],
+      resolve: {
+        extensions: [".js", ".json", ".css", ".sass"],
       },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"],
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".js", ".json", ".css", ".sass"],
-  },
-  devtool: "inline-source-map",
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: "Not Trello",
-      filename: "index.html",
-      template: "./src/index.html",
-      inject: "body",
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      ignoreOrder: false,
-    }),
-  ],
-  devServer: {
-    contentBase: "./dist",
-    host: "localhost",
-    port: "3000",
-    historyApiFallback: true,
-    hot: true,
-    open: true,
-  },
+      devtool: "inline-source-map",
+      plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+          title: "Not Trello",
+          filename: "index.html",
+          template: "./src/index.html",
+          inject: "body",
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new MiniCssExtractPlugin({
+          filename: '[name].css',
+          ignoreOrder: false,
+        }),
+      ],
+      devServer: {
+        contentBase: "./dist",
+        host: "localhost",
+        port: "3000",
+        historyApiFallback: true,
+        hot: true,
+        open: true,
+      },
+    }
+  }
+  if (argv.mode === 'production') {
+    config = {
+      mode: "production",
+      entry: {
+        app: "./src/index.js",
+      },
+      output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "public"),
+        publicPath: "/"
+      },
+      optimization: {
+        splitChunks: {
+          cacheGroups: {
+            styles: {
+              name: 'styles',
+              test: /\.css$/,
+              chunks: 'all',
+              enforce: true,
+            },
+          },
+        },
+      },
+      module: {
+        rules: [
+          {
+            test: /\.js?$/,
+            exclude: "/node_modules/",
+            use: ["babel-loader"],
+          },
+          {
+            test: /\.scss$/,
+            use: [MiniCssExtractPlugin.loader,
+              "css-loader",
+              {
+                loader: "sass-loader",
+              },
+              {loader: "required-loader"}
+            ],
+          },
+          {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ["file-loader"],
+          },
+          {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: ["file-loader"],
+          }
+        ]
+      },
+      resolve: {
+        extensions: [".js", ".json", ".css", ".sass"],
+      },
+      devtool: "inline-source-map",
+      plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+          title: "Not Trello",
+          filename: "index.html",
+          template: "./src/index.html",
+          inject: "body",
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new MiniCssExtractPlugin({
+          filename: '[name].css',
+          ignoreOrder: false,
+        }),
+      ],
+    }
+  }
+  return config;
 }
